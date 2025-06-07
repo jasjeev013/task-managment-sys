eddf2024fddab6463ab7c5984e522255ac5db802
Jasjeev <jasjeev99@gmail.com>
2025-06-07 18:13:43 +0530

    fix: Update CORS origin to use explicit array of allowed origins

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.ORIGIN_CORS,
+  origin: ["https://task-managment-sys.vercel.app","http://localhost:5173"],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
