03fa92f43ff06b400cc27a412ff8269cd5a3ac36
Jasjeev <jasjeev99@gmail.com>
2025-06-07 18:42:10 +0530

    fix: Remove redundant CORS options middleware call

--- server/index.js
+++ server/index.js
@@ -16,12 +16,7 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
-app.options('*', cors({
-  origin: ["https://task-managment-sys.vercel.app", "http://localhost:5173"],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+
 app.use(helmet());
 app.use(morgan('dev'));
 app.use(express.json());
